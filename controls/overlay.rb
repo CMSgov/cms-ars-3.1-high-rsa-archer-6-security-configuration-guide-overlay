c00a7053b271def03143939bee16fb92a6786f35
Daniel Haynes <daniel.haynes@cms.hhs.gov>
2018-12-05 07:39:46 -0500

    Update overlay.rb
    adding overlay support for 1.8.

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -63,6 +63,27 @@ include_controls 'archer-baseline' do
     	its('default_administrative_user.PasswordChangeInterval') { should cmp <= 60 }
     	its('general_user_parameter.PasswordChangeInterval') { should cmp <= 60 }
     	its('archer_services_parameter.PasswordChangeInterval') { should cmp <= 60 }
+    end
   end
+  
+  
+  control 'rsa-archer-1.8' do
+    desc 'Passwords must be prohibited from reuse for a minimum of 12 generations.'
+    tag 'check': 'In security parameters, check if PreviousPasswordsDisallowed = 12'
+    tag 'fix': 'In security parameters, set PreviousPasswordsDisallowed = 12'
+	    
+    archer_api_helper = archer(url: attribute('url'),
+                             instancename: attribute('instancename'),
+                             user_domain: attribute('user_domain'),
+                             username: attribute('username'),
+                             password: attribute('password'),
+                             ssl_verify: attribute('ssl_verify'))
+
+    describe archer_api_helper do
+      its('default_administrative_user.PreviousPasswordsDisallowed') { should cmp >= 12 }
+      its('general_user_parameter.PreviousPasswordsDisallowed') { should cmp >= 12 }
+      its('archer_services_parameter.PreviousPasswordsDisallowed') { should cmp >= 12 }
+    end
   end
+end
   
