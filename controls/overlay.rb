6b1c09564a2f165cabeb767bd30d3a6a124ed9f1
Daniel Haynes <daniel.haynes@cms.hhs.gov>
2018-12-05 15:08:12 -0500

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -4,32 +4,32 @@ include_controls 'archer-baseline' do
 
   control 'rsa-archer-1.1' do
     desc 'Passwords must be a minimum of 15 characters in length.'
-    tag 'check': 'In security parameters, check if MinPasswordLength = 15'
-    tag 'fix': 'In security parameters, set MinPasswordLength = 15'
+    desc 'check', 'In security parameters, check if MinPasswordLength = 15'
+    desc 'fix', 'In security parameters, set MinPasswordLength = 15'
   end
   
   control 'rsa-archer-1.2' do
     desc 'When passwords are changed or new passwords are established, the new password must contain at least one alpha characters.'
-    tag 'check': 'In security parameters, check if AlphaCharsRequired = 1'
-    tag 'fix': 'In security parameters, set AlphaCharsRequired = 1'
+    desc 'check', 'In security parameters, check if AlphaCharsRequired = 1'
+    desc 'fix', 'In security parameters, set AlphaCharsRequired = 1'
   end
   
   control 'rsa-archer-1.7' do
     desc 'Existing passwords must be restricted to a 60-day maximum lifetime.'
-    tag 'check': 'In security parameters, check if PasswordChangeInterval = 60'
-    tag 'fix': 'In security parameters, set PasswordChangeInterval = 60'
+    desc 'check', 'In security parameters, check if PasswordChangeInterval = 60'
+    desc 'fix', 'In security parameters, set PasswordChangeInterval = 60'
   end
     
   control 'rsa-archer-1.8' do
     desc 'Passwords must be prohibited from reuse for a minimum of 12 generations.'
-    tag 'check': 'In security parameters, check if PreviousPasswordsDisallowed = 12'
-    tag 'fix': 'In security parameters, set PreviousPasswordsDisallowed = 12'
+    desc 'check', 'In security parameters, check if PreviousPasswordsDisallowed = 12'
+    desc 'fix', 'In security parameters, set PreviousPasswordsDisallowed = 12'
   end
   
   control 'rsa-archer-1.11' do
     desc 'The operating system must initiate a session time-out after a 15 minute period of inactivity.'
-    tag 'check': 'In security parameters, check if SessionTimeout = 15.'
-    tag 'fix': 'In security parameters, set SessionTimeout = 15.'
+    desc 'check', 'In security parameters, check if SessionTimeout = 15.'
+    desc 'fix', 'In security parameters, set SessionTimeout = 15.'
   end
 end
   
